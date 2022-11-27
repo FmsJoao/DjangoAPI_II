bde9c6d77f8f918e63987e93345f9fafa92a6a02
FmsJoao <joao.victorfmaciel@gmail.com>
2022-11-27 20:00:01 -0300

    Adding field validation

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -5,4 +5,23 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
-    
+
+    def validate_cpf (self,cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError("The cpf must have 11 digits")
+        return cpf
+
+    def validate_nome (self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("The name can only have letters")
+        return nome
+
+    def validate_rg (self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("The RG must have 9 digits")
+        return rg
+
+    def validate_celular(self, celular):
+        if len(celular) <11:
+            raise serializers.ValidationError("The celular must have at least 11 digits")
+        return celular
