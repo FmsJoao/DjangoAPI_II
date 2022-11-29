2e31e01c00db4967daa28ce741c41955b64167ab
FmsJoao <joao.victorfmaciel@gmail.com>
2022-11-28 23:23:04 -0300

    Making the code cleaner

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,27 +1,23 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import cpf_valido, nome_valido, rg_valido
+
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
 
-    def validate_cpf (self,cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("The cpf must have 11 digits")
-        return cpf
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf': "The cpf must have 11 digits"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome': "The name can only have letters"})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg': "The RG must have 9 digits"})
+        return data
 
-    def validate_nome (self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError("The name can only have letters")
-        return nome
-
-    def validate_rg (self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError("The RG must have 9 digits")
-        return rg
-
-    def validate_celular(self, celular):
-        if len(celular) <11:
-            raise serializers.ValidationError("The celular must have at least 11 digits")
-        return celular
+    # def validate_celular(self, celular):
+    # if len(celular) <11:
+    # raise serializers.ValidationError("The celular must have at least 11 digits")
+    # return celular
